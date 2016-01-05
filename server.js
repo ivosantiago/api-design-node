7051f5dee07dbb4fb55fe0386974b810fa65d7bb
Ivo Santiago <ivosantiago@gmail.com>
2016-01-05 18:06:25 -0200

    My solution

--- server.js
+++ server.js
@@ -2,4 +2,20 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/data', function (req, res) {
+  res.json(jsonData);
+})
+
+var port = 3000;
+app.listen(port, function () {
+  console.log('Listening ' + port);
+})
