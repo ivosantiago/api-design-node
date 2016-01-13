92f73576115514997f6369854c788cc8ca98e7a3
Ivo Santiago <ivosantiago@gmail.com>
2016-01-12 23:23:13 -0200

    my solution

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var api = require('./api/api');
+var logger = require('./util/logger');
 
 // setup the app middlware
 require('./middleware/appMiddlware')(app);
@@ -8,6 +9,14 @@ require('./middleware/appMiddlware')(app);
 // setup the api
 app.use('/api/', api);
 // set up global error handling
+
+app.use(function (err, req, res, next) {
+  if (err) {
+    logger.log('Error');
+    req.send(err);
+  }
+  next();
+})
 
 // export the app for testing
 module.exports = app;
